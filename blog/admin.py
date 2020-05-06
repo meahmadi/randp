1888965bfae6533aeaba030bf5eef78acce3118d
meisam <me.ahmadi@gmail.com>
2020-05-06 17:14:29 +0430

    new files

---
+++ blog/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+
+# Register your models here.
+from .models import Post , Comment
+
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+  
+admin.site.register(Post, PostAdmin)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
